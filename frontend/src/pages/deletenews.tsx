d99f71db41bbe990df92c631b7db1f764adf39de
PHDeveloper <phbm@hotmail.com>
2020-10-27 03:45:20 -0300

    Removendo variável não utilizada

--- frontend/src/pages/deletenews.tsx
+++ frontend/src/pages/deletenews.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../components/Sidebar'
 import Newscard from '../pages/pagenews'
 import { useParams, useHistory } from 'react-router-dom';
